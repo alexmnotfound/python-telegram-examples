7a886c333292acf28d5464dd18de99507b6397ec
adalexander <47927490+alexmnotfound@users.noreply.github.com>
2021-08-21 13:24:15 -0300

    Bots con teclados modificados

--- keyboardDualBot.py
+++ keyboardDualBot.py
@@ -3,15 +3,27 @@ from telegram.ext import Updater, CommandHandler, CallbackQueryHandler
 from telegram import InlineKeyboardButton, InlineKeyboardMarkup
 from config.auth import telegramBotToken
 
-# Armo Teclado
-keyboard = [[InlineKeyboardButton("Opción 1", callback_data='option1'),
+# Armo Teclado Principal
+
+welcomeMsg = 'Hola! Elegí que opción querés.'
+
+keyboard1 = [[InlineKeyboardButton("Opción 1", callback_data='option1'),
              InlineKeyboardButton("Opción 2", callback_data='option2')],
 
-            [InlineKeyboardButton("Opción 3", callback_data='option3'),
+            [InlineKeyboardButton("Más opciones >>", callback_data='moreOptions'),
              InlineKeyboardButton("Ayuda", callback_data='help')]
             ]
 
-keyboard = InlineKeyboardMarkup(keyboard)
+keyboard1 = InlineKeyboardMarkup(keyboard1)
+
+# Armo Teclado Secundario
+keyboard2 = [[InlineKeyboardButton("Opción 3", callback_data='option3'),
+             InlineKeyboardButton("Opción 4", callback_data='option4')],
+
+            [InlineKeyboardButton("<< Atrás", callback_data='backToKb1')]
+            ]
+
+keyboard2 = InlineKeyboardMarkup(keyboard2)
 
 
 def button(update, context):
@@ -26,21 +38,27 @@ def button(update, context):
     query.answer()
 
     if query.data == 'option1':
-        query.edit_message_text(text=f"Elegiste la opción 1", reply_markup=keyboard)
+        query.edit_message_text(text=f"Elegiste la opción 1", reply_markup=keyboard1)
     elif query.data == 'option2':
-        query.edit_message_text(text=f"Elegiste la opción 2", reply_markup=keyboard)
-    elif query.data == 'option3':
-        query.edit_message_text(text=f"Elegiste la opción 3", reply_markup=keyboard)
+        query.edit_message_text(text=f"Elegiste la opción 2", reply_markup=keyboard1)
+    elif query.data == 'moreOptions':
+        query.edit_message_text(text=f"Menú secundario", reply_markup=keyboard2)
     elif query.data == 'help':
         query.edit_message_text(text=f"Soy un teclado de ejemplo.\n"
-                                     f"Tocá cualquier tecla para simular una elección!", reply_markup=keyboard)
+                                     f"Tocá cualquier tecla para simular una elección!", reply_markup=keyboard1)
+    elif query.data == 'option3':
+        query.edit_message_text(text=f"Elegiste la opción 3", reply_markup=keyboard2)
+    elif query.data == 'option4':
+        query.edit_message_text(text=f"Elegiste la opción 4", reply_markup=keyboard2)
+    elif query.data == 'backToKb1':
+        query.edit_message_text(text=welcomeMsg, reply_markup=keyboard1)
 
 
 def start(update, context):
     """
     Defino función y texto al iniciar el bot
     """
-    update.message.reply_text('Hola! Elegí que opción querés.',  reply_markup=keyboard)
+    update.message.reply_text(welcomeMsg,  reply_markup=keyboard1)
 
 
 def main():
